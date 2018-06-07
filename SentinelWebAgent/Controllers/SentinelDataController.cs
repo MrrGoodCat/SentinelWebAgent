fca88dd2ba5ccde3b1d119ec42e9d886fce793bb
MrrGoodCat <mrrgoodcat@gmail.com>
2018-06-08 01:27:25 +0300

    Remove xml Formatter

--- SentinelWebAgent/Controllers/SentinelDataController.cs
+++ SentinelWebAgent/Controllers/SentinelDataController.cs
@@ -20,11 +20,96 @@ namespace SentinelWebAgent.Controllers
         }
 
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities employeeDBEntities = new EmployeeDBEntities())
             {
-                return employeeDBEntities.Employees.FirstOrDefault(e => e.ID == id);
+                
+                var employee = employeeDBEntities.Employees.FirstOrDefault(e => e.ID == id);
+                if (employee != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, employee);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID: " + id + " not found.");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post ([FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities employeeDBEntities = new EmployeeDBEntities())
+                {
+                    employeeDBEntities.Employees.Add(employee);
+                    employeeDBEntities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }            
+        }
+
+        public HttpResponseMessage Delete (int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities employeeDBEntities = new EmployeeDBEntities())
+                {
+                    var result = employeeDBEntities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (result == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID = " + id + " not found to delete.");
+                    }
+                    else
+                    {
+                        employeeDBEntities.Employees.Remove(result);
+                        employeeDBEntities.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities employeeDBEntities = new EmployeeDBEntities())
+                {
+                    var result = employeeDBEntities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (result == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID = " + id + " not found to update.");
+                    }
+                    else
+                    {
+                        result.FirstName = employee.FirstName;
+                        result.LastName = employee.LastName;
+                        result.Gender = employee.Gender;
+                        result.Salary = employee.Salary;
+                        employeeDBEntities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
