bda0d938057ea3af07332136dc966f90a2497edf
MrrGoodCat <mrrgoodcat@gmail.com>
2018-06-11 00:43:29 +0300

    javascript is working

--- SentinelWebAgent/App_Start/WebApiConfig.cs
+++ SentinelWebAgent/App_Start/WebApiConfig.cs
@@ -19,9 +19,9 @@ namespace SentinelWebAgent
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
             //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
